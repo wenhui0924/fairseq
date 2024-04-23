ca2f71814a97329cb346b71a52d3ae63c6cf677e
Shuming Ma <shumma@microsoft.com>
2024-04-23 11:13:10 -0700

    group zero

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -159,7 +159,11 @@ class Trainer(object):
             else:
                 # self.expert_group = get_zero_group(torch.cuda.device_count() * 2)
                 # self.data_parallel_process_group
-                self.expert_group = self.data_parallel_process_group
+                # self.expert_group = self.data_parallel_process_group
+                if self.cfg.distributed_training.zero_group_size > 0:
+                    self.expert_group = get_zero_group(torch.cuda.device_count() * self.cfg.distributed_training.zero_group_size)
+                else:
+                    self.expert_group = self.data_parallel_process_group
 
         # TODO(myleott): support tpu
         if self.cuda and self.data_parallel_world_size > 1:
